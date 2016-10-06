c7bf8822f062be5933961904d216115588910f65
Miguel Mulas Andrés <Miguel Mulas Andrs>
2016-10-07 01:09:54 +0200

    Prueba node

---
+++ pruebaynave.js
@@ -0,0 +1,143 @@
+40+2
+34+21
+var coord = {x=1,y=0}
+var coord{x:1,y:0}
+;
+
+var coord {x:1,y:0};
+var coord = {x:1,y:0};
+
+var coord = {x:1,y:0};
+coord
+coord.x
+coord.y
+console.log ('hola mundo');
+console.log(coord)
+console.log (68/2);
+console.log('Coordenada x:' coord.x);
+console.log('Coordenada x:',coord.x);
+typeof true
+typeof 'quiero aprobar PVLI'
+typeof 67
+12.0===12
+true === false
+"hola"==="Hola"
+var vacio={};
+vacio
+var peperoni={};
+vacio===peperoni
+var jesus = peperoni;
+jesus===peperoni
+var point = {x:10,y:10};
+point['x']
+point.x = 15
+point['x']
+var label=point.label
+point.label = 'california';
+point
+var menu = ['ataque','defensa','inventario'];
+menu[0];
+menu.lenght
+menu.lenght;
+menu.length;
+menu.push('magia')
+menu
+menu.pop;
+menu
+menu.pop
+menu.pop();
+menu
+menu.splice(2,0,'magia');
+menu
+menu.splice(2,2,'comida');
+menu
+menu[3]='inventario'
+menu
+menu[2]='magia';
+menu
+var item = menu[10];
+item ='secreto';
+menu
+menu.lenght+
+menu.lenght;
+menu.length;
+menu[10] = 'secreto';
+menu.length;
+menu
+var hero ={espada:null,defensa:null, nombre:null};
+hero
+hero.espada = 'gungnir ardiente afilada';
+hero
+hero.nombre = 'Sir Pelele 34';
+hero
+hero.nombre = 'Pancho';
+hero
+var inventario = hero.inventario;
+hero
+var inventario =hero.inventario
+inventario = [item1:null,item2:null];
+inventario = ['item1','item2'];
+hero
+hero.inventario = ['item1','item2'];
+hero
+hero.inventario[1];
+hero.nombre
+hero.inventario[0]= 'sangre de malvavisco salvaje';
+hero
+var nave enemiga = {nombre:'GXenofighter12',direccion:'derecha',posicion:{x:0,y:0},disparo:'KATAPUM'};
+var naveEnemiga = {nombre:'GXenofighter12',direccion:'derecha',posicion:{x:0,y:0},disparo:'KATAPUUUUMBA'};
+naveEnemiga
+naveEnemiga.posicion.x = 100;
+var moveleft = naveEnemiga.moveleft;
+naveEnemiga.moveleft = function(){console.log('move left');};
+naveEnemiga
+naveEnemiga.moveleft
+naveEnemiga.moveleft=console.log ('moveleft');
+naveEnemiga.moveleft;
+naveEnemiga
+nave enemiga.moveleft = function(){console.log('IZQ');};
+naveEnemiga.moveleft=function(){console.log('IZQ');};
+naveEnemiga
+naveEnemiga.moveleft();
+naveEnemiga.disparo;
+naveEnemiga.disparo = function () {console.log('baaang');};
+naveEnemiga.disparo();
+var moveRight = naveEnemiga.moveRight;
+naveEnemiga.moveRight=function(){console.log('DER');};
+naveEnemiga
+naveEnemiga.moveRight = function(){this.posicion.x+=2;};
+naveEnemiga.moveRight
+naveEnemiga
+naveEnemiga
+naveEnemiga.moveRight;
+naveEnemiga
+naveEnemiga.moveRight = function(){this.posicion.x +=2;};
+naveEnemiga
+naveEnemiga.moveRIGHT
+naveEnemiga.moveRight
+naveEnemiga
+naveEnemiga.moveRight=function(){naveEnemiga.posicion.x+=2;};
+naveEnemiga
+naveEnemiga.moveRight
+naveEnemiga
+naveEnemiga.posicion.x
+naveEnemiga.disparo
+naveEnemiga.moveRight();
+naveEnemiga
+naveEnemiga.moveleft();
+naveEnemiga.moveleft=function(){this.posicion.x-=2;};
+naveEnemiga
+naveEnemiga.moveleft();
+naveEnemiga
+naveEnemiga.moveleft=function(){this.posicion.x-=2,this.direccion = 'izquierda';};
+naveEnemiga.moveleft();
+naveEnemiga
+naveEnemiga.moveRight=function(){this.posicion.x+=2,this.direccion = 'derecha'};
+naveEnemiga.moveRight();
+naveEnemiga
+//todos los comandos probados y naveEnemiga modelada
+naveEnemiga
+//comandos probados, naveEnemiga modelada FIN
+naveEnemiga
+//Comandos dados en clase probados y modelado de nave enemiga terminado
+naveEnemiga
